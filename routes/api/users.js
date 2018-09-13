7209ac05031018d724ff9e9b80ac1f29a3425cec
ChangLiu <lclc731@gmail.com>
2018-09-13 00:18:18 -0700

    add register api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+// load user model
+const User = require("../../models/User");
+
 // @route GET api/posts/test
 // @desc  Tests post route
 // @access  Public
 router.get("/test", (req, res) => res.send("test"));
+
+// @route POST api/posts/register
+// @desc  Register a user, if username exists return 400, else
+//        create a user saved to DB
+// @access  Public
+
+// TODO task 1, task 2, task 3
+router.post("/register", (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        username: "username already exists"
+      });
+    } else {
+      const newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+      });
+      newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const doctors = require("./routes/api/doctors");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require("./config/key").mongoURI;
